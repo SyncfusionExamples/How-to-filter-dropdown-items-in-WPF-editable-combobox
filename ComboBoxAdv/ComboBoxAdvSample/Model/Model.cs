835a8afe2a8fb5f2b6c95d03635404e89d05770d
duraimurugan.vedagiri <duraimurugna.v>
2020-05-05 16:57:05 +0530

    WPF-52422 How to filter dropdown items in WPF editable ComboBoxAdv?

---
+++ ComboBoxAdv/ComboBoxAdvSample/Model/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxAdvSample
+{
+    public class Model
+    {
+        public Model(string name)
+        {
+            this.name = name;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
---
+++ ComboBoxAdv/ComboBoxAdvSample/ViewModel/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxAdvSample
+{
+    public class ViewModel
+    {
+        public List<Model> List
+        {
+            get;
+            set;
+        }
+
+        public ViewModel()
+        {
+            List = new List<Model>();
+            List.Add(new Model("Alaska"));
+            List.Add(new Model("Arizona"));
+            List.Add(new Model("Colorado"));
+            List.Add(new Model("Connecticut"));
+            List.Add(new Model("Delaware"));
+            List.Add(new Model("Florida"));
+            List.Add(new Model("Georgia"));
+            List.Add(new Model("Hawaii"));
+            List.Add(new Model("Idaho"));
+            List.Add(new Model("Illinois"));
+            List.Add(new Model("Indiana"));
+            List.Add(new Model("Iowa"));
+            List.Add(new Model("Kansas"));
+        }
+    }
+}
